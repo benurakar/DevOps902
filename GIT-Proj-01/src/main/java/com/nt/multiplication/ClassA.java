883e1cf827c2de4064c5aa836c96331275da1d51
bennu <bennu@Yuvaraj>
2024-02-27 16:40:18 +0530

    push the code

---
+++ GIT-Proj-01/src/main/java/com/nt/multiplication/ClassA.java
@@ -0,0 +1,9 @@
+package com.nt.multiplication;
+
+public class ClassA 
+{
+	public int mul(int x, int y)
+	{
+		return x+y;
+	}
+}
--- GIT-Proj-01/src/main/java/com/nt/service/App.java
+++ GIT-Proj-01/src/main/java/com/nt/service/App.java
@@ -1,12 +1,28 @@
 package com.nt.service;
 
+import com.nt.multiplication.*;
+
 public class App 
 {
+	public int method(int x, int y)
+	{
+		ClassA obj= new ClassA();
+		int result=obj.mul(2,10);
+		System.out.println(result);
+		System.out.println(obj.mul(10, 20)+2);
+		System.out.println(x);
+		return y;
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Yuvaraj");
+        App obj1=new App();
+        int result=obj1.method(2, 8);
+        //new App().method(2, 4);
+        System.out.println(result);
     }
 }
